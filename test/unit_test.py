e4438b3feb51e30929c2ba552145b7df75d14391
William Tidelund <william.tidelund@gmail.com>
2023-10-10 15:01:58 +0200

    added reading wav and yaml from test

--- test/unit_test.py
+++ test/unit_test.py
@@ -1,7 +1,8 @@
 import unittest
 import os
+import yaml
 
-PATH_TO_DATA = "data"
+PATH_TO_DATA = "data/"
 
 class TestStringMethods(unittest.TestCase):
 
@@ -9,7 +10,9 @@ class TestStringMethods(unittest.TestCase):
         for filename in os.listdir(PATH_TO_DATA):
             if filename.endswith(".wav"):
                 print(filename)
-                continue
+                with open(PATH_TO_DATA + filename[:-3]+'yaml', 'r') as file:
+                    data = yaml.safe_load(file)
+                    print(data['clap_pattern'])
             else:
                 continue
 
