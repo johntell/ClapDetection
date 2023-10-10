bbc86da034d11f16691c7f9ab9fcb3781b482d4c
William Tidelund <william.tidelund@gmail.com>
2023-10-10 15:12:34 +0200

    extended test, added todo

--- test/unit_test.py
+++ test/unit_test.py
@@ -1,6 +1,8 @@
 import unittest
 import os
 import yaml
+from scipy.io.wavfile import read
+import numpy as np
 
 PATH_TO_DATA = "data/"
 
@@ -13,6 +15,18 @@ class TestStringMethods(unittest.TestCase):
                 with open(PATH_TO_DATA + filename[:-3]+'yaml', 'r') as file:
                     data = yaml.safe_load(file)
                     print(data['clap_pattern'])
+
+                    # Read the wave file
+                    sample_rate, audio_data = read(PATH_TO_DATA+filename)
+
+                    # Convert to mono if the audio file has 2 channels
+                    if len(audio_data.shape) == 2:
+                        audio_data = np.mean(audio_data, axis=1, dtype=int)
+
+                    print(len(audio_data), sample_rate)
+                    #TOdo: call clap_detect_alg1 and pattern_detect with BUFFER = 1024 samples from audio_data at a time until end of audio_data.
+                    # padd audio_data with 0's if not mylitple of BUFFER =1024
+                    # Check if clap_pattern from yaml matches with the calculated
             else:
                 continue
 
